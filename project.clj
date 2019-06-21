c5b0c4ea5dc3e7ed422bc8bb4c3f4e90f7812d10
George Narroway <george.narroway@me.com>
2019-06-21 22:55:40 +0800

    release config

--- project.clj
+++ project.clj
@@ -1,13 +1,17 @@
 (defproject hato "0.1.0-SNAPSHOT"
   :description "An HTTP client for Clojure, wrapping JDK 11's HttpClient."
   :url "https://github.com/gnarroway/hato"
-  :license {:name "The MIT License"
-            :url "http://opensource.org/licenses/mit-license.php"
+  :license {:name         "The MIT License"
+            :url          "http://opensource.org/licenses/mit-license.php"
             :distribution :repo}
-  :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
-                                    :username :env/clojars_user
-                                    :password :env/clojars_pass
-                                    :sign-releases false}]]
+  :deploy-repositories [["snapshots" {:url           "https://clojars.org/repo"
+                                      :username      :env/clojars_user
+                                      :password      :env/clojars_pass
+                                      :sign-releases false}]
+                        ["releases" {:url           "https://clojars.org/repo"
+                                     :username      :env/clojars_user
+                                     :password      :env/clojars_pass
+                                     :sign-releases false}]]
   :plugins [[lein-cljfmt "0.6.4"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.1"]
                                   [cheshire "5.8.1"]
