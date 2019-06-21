6acef23df7ed9a84498ffc111f7f0788ae3b945d
George Narroway <george.narroway@me.com>
2019-06-21 23:01:25 +0800

    release config

--- project.clj
+++ project.clj
@@ -13,6 +13,14 @@
                                      :password      :env/clojars_pass
                                      :sign-releases false}]]
   :plugins [[lein-cljfmt "0.6.4"]]
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag" "v" "--no-sign"] ; disable signing and add "v" prefix
+                  ["deploy"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.1"]
                                   [cheshire "5.8.1"]
                                   [com.cognitect/transit-clj "0.8.313"]]}})
