62de2c9466a0c7aa776482be0c9004fb9b5e305a
George Narroway <george.narroway@me.com>
2019-06-21 23:09:50 +0800

    release config

--- project.clj
+++ project.clj
@@ -4,23 +4,19 @@
   :license {:name         "The MIT License"
             :url          "http://opensource.org/licenses/mit-license.php"
             :distribution :repo}
-  :deploy-repositories [["snapshots" {:url           "https://clojars.org/repo"
+  :deploy-repositories [["clojars" {:url           "https://clojars.org/repo"
                                       :username      :env/clojars_user
                                       :password      :env/clojars_pass
-                                      :sign-releases false}]
-                        ["releases" {:url           "https://clojars.org/repo"
-                                     :username      :env/clojars_user
-                                     :password      :env/clojars_pass
-                                     :sign-releases false}]]
-  :plugins [[lein-cljfmt "0.6.4"]]
+                                      :sign-releases false}]]
   :release-tasks [["vcs" "assert-committed"]
                   ["change" "version" "leiningen.release/bump-version" "release"]
                   ["vcs" "commit"]
                   ["vcs" "tag" "v" "--no-sign"] ; disable signing and add "v" prefix
-                  ["deploy"]
+                  ["deploy" "clojars"]
                   ["change" "version" "leiningen.release/bump-version"]
                   ["vcs" "commit"]
                   ["vcs" "push"]]
+  :plugins [[lein-cljfmt "0.6.4"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.1"]
                                   [cheshire "5.8.1"]
                                   [com.cognitect/transit-clj "0.8.313"]]}})
